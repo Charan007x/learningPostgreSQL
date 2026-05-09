2ab6dab401bfc07921f901e61c51b44e751d603d
Sai Charan Gottipalli <saicharangottipalli@gmail.com>
2026-05-09 18:41:23 +0530

    Fixing redis prod issue

--- src/config/redis.js
+++ src/config/redis.js
@@ -2,12 +2,7 @@ import redis from "redis";
 import dotenv from "dotenv";
 dotenv.config();
 const redisClient = redis.createClient({
-    username: process.env.REDIS_USERNAME,
-    password: process.env.REDIS_PASSWORD,
-    socket: {
-        host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT
-    }
+    url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
 });
 
 redisClient.on('error', (err) => console.error('Redis Client Error', err));
